7baf9e8df08a2fff55d061deb308280a8d6a0cbe
impAct <annguyenxax@gmail.com>
2023-11-29 11:01:21 +0700

    add service api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { environment } from './../environments/environment';
+import {environment} from "../environments/environment.development";
+
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import {DataTableComponent} from "../data-table/data-table.component";
+import {DataTableComponent} from "./components/data-table/data-table.component";
 
 @NgModule({
   declarations: [
